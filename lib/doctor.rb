881c3d42a0db5b8a55ea5e68dc1881ac619a9467
Janusz Szubert <theinvalidnonce@gmail.com>
2017-09-07 21:57:34 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -4,17 +4,22 @@ class Doctor
 
   def initialize(name)
     @name = name
-
-
+    @appointments = []
   end
 
 
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
 
+  def appointments
+    @appointments
+  end
 
-
-
-
-
+  def patients
+    @appointments.map { |appointment| appointment.patient }
+  end
 
 
 end
--- lib/genre.rb
+++ lib/genre.rb
@@ -19,8 +19,5 @@ class Genre
   def artists
     @songs.map { |song| song.artist }
   end
-
-
-
-
+  
 end
